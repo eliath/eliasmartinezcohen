b057eb3465f4bf87fbfc5a5640acae6c0c2016b9
eliath <eliasmartinezcohen@gmail.com>
2015-12-16 01:44:00 -0500

    Clean mobile design & content

--- src/js/_mobile.js
+++ src/js/_mobile.js
@@ -3,39 +3,26 @@ _mobile  = {
 	init: function init() {
 		this.$el = $('body').attr('id', 'mobile').empty();
 		this.render();
-
-		//force portrait
-		this.fixOrientation();
-		window.onorientationchange = this.fixOrientation;
 	},
 
 	render: function render() {
 		var _this = this;
 		this.$el.load('mobile.html', function() {
-			$('.contact').html(_site.obfuscateEmail());
+			var email = _site.getEmail();
+			$('.contact').append($('<a>', {href:'mailto:'+email, html:email}));
 			$('.more').click($.proxy(_this.openDrawer, _this));
 		});
 	},
 
 	openDrawer: function openDrawer() {
-		$('.info').addClass('open');
+		$('.links').addClass('open');
 		$('.main').addClass('blurred');
 		$('.back-btn').click($.proxy(this.closeDrawer, this));
 	},
 
 	closeDrawer: function closeDrawer() {
-		console.log('X helooooo?');
+		console.log('X helooooo?'); 
 		$('.main').removeClass('blurred');
-		$('.info').removeClass('open');
+		$('.links').removeClass('open');
 	},
-
-	fixOrientation: function fixOrientation() {
-		console.log('OREINTATION:', window.orientation);
-		if (window.orientation % 180 !== 0) {
-			$('#wrap').addClass('fix-rotate');	
-		} else {
-			$('#wrap').removeClass('fix-rotate');	
-		}
-	}
-
 };
